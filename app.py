dd98b9b155687c24f9ab77475f15c9e28da1e9c4
Christine Maynard <christinekrm02@gmail.com>
2020-10-05 17:27:12 -0400

    Added form and functionality. TODO: Add bootstrap styles to other pages, complete login page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #Environment set-up
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+#Use terminal, activate python, import secrets, call secrets.token_hex(number of bytes to generate) to create a randomized secret key
+app.config['SECRET_KEY'] = '24b5d2d61f2b6eb1f45c9767d52aa55a'
 
 posts = [
     {
@@ -25,6 +28,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', name='Christine')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
