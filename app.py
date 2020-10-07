b3863feaa5af5733a6bfb43a62b410894988b242
Christine Maynard <christinekrm02@gmail.com>
2020-10-07 14:03:48 -0400

    Created database, needed to import os and create path for basedir to create database. TODO: Create package structure

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 #Environment set-up
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+import os
+
 app = Flask(__name__)
 #Use terminal, activate python, import secrets, call secrets.token_hex(number of bytes to generate) to create a randomized secret key
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = '24b5d2d61f2b6eb1f45c9767d52aa55a'
+#SQL database will be a local file for now.
+#database connection setup. Assigns sqlite path, base directory and name of application to sqlite database. Note that this app.config is pointing to the instantiated Flask class (our app)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+#Create instance of database
+db = SQLAlchemy(app)
+
+#Create users (as classes)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    #1:many == user:posts
+    posts = db.relationship('Post', backref='author', lazy=True)
+    #Output when a user obj is printer
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     title= db.Column(db.String(100), nullable=False)
+     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+     content = db.Column(db.Text, nullable=False)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+     def __repr__(self):
+         return f"Post('{self.title}', '{self.date_posted}')"
+
+
 
 posts = [
     {
@@ -51,3 +86,6 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+  
+
