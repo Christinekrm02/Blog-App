9edbf64cd1b07dc897adad350747ddfb05e7a890
Christine Maynard <christinekrm02@gmail.com>
2020-10-10 18:00:08 -0400

    Added user authrntication to verify login credentials/verify if user already has an account/verify if login demographics already exists. Discovery: If having problems accessing database tables, recreate database from command line. Also- set app.config[TESTING] to false in init file if having 404 errors associated with login functionality. TODO- add landing page, account information by user, and fix flash messages. Whew

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,16 +1,25 @@
 #Environment set-up
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
 #Use terminal, activate python, import secrets, call secrets.token_hex(number of bytes to generate) to create a randomized secret key
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = '24b5d2d61f2b6eb1f45c9767d52aa55a'
+app.config['TESTING'] = False
 #SQL database will be a local file for now.
 #database connection setup. Assigns sqlite path, base directory and name of application to sqlite database. Note that this app.config is pointing to the instantiated Flask class (our app)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 #Create instance of database
 db = SQLAlchemy(app)
+#pwrd encryption
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 #Use Python classes to represent the forms
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password= PasswordField('Password', validators=[DataRequired()])
-    confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
+   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+   email = StringField('Email', validators=[DataRequired(), Email()])
+   password= PasswordField('Password', validators=[DataRequired()])
+   confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+   submit = SubmitField('Sign Up')
+#Do not alter this syntax. FlaskForm checks for the validate keyword
+   def validate_username(self, username):
+     user = User.query.filter_by(username=username.data).first()
+     if user:
+       raise ValidationError('Looks like someone got to it first! That username already exists.')
+   def validate_email(self, email):
+     user = User.query.filter_by(email=email.data).first()
+     if user:
+       raise ValidationError('Email is already taken.')
+     
 class LoginForm(FlaskForm):
     #User can login with uname or email
    # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
