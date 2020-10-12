789e3a8ccc970f06a9650c3ef4aaa48df94562a4
Christine Maynard <christinekrm02@gmail.com>
2020-10-12 18:34:35 -0400

    Began adding user account profile picture functionality. Incomplete

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -28,3 +30,23 @@ class LoginForm(FlaskForm):
   #remeber field stores cookie of user's id and "remebers" them for each login
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+#To update username and email address
+class UpdateAccountForm(FlaskForm):
+   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+   email = StringField('Email', validators=[DataRequired(), Email()])
+   picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+   submit = SubmitField('Update')
+#Do not alter this syntax. FlaskForm checks for the validate keyword
+   def validate_username(self, username):
+     if username.data != current_user.username:
+       user = User.query.filter_by(username=username.data).first()
+       if user:
+         raise ValidationError('Looks like someone got to it first! That username already exists.')
+   def validate_email(self, email):
+     if email.data != current_user.email:
+       user = User.query.filter_by(email=email.data).first()
+       if user:
+         raise ValidationError('Email is already taken.')
+                        
+     
