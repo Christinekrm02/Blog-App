ce1837b01a5809f288a7f6e5501fe4af43539656
Christine Maynard <christinekrm02@gmail.com>
2020-10-16 21:05:06 -0400

    Completed user update/remove image. TODO: Update layout, create homepage, do UPDATE/DELETE blog posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+#Allows user to update user info/picture
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
@@ -37,8 +38,9 @@ class UpdateAccountForm(FlaskForm):
    email = StringField('Email', validators=[DataRequired(), Email()])
    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
    submit = SubmitField('Update')
-#Do not alter this syntax. FlaskForm checks for the validate keyword
+#Do not alter this syntax.
    def validate_username(self, username):
+     #'If username that user supplies is not the same as a current record of that username...'
      if username.data != current_user.username:
        user = User.query.filter_by(username=username.data).first()
        if user:
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -27,7 +30,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', name='Christine')
-
+#add methods when collecting data!
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user .is_authenticated:
@@ -65,16 +68,42 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#Change name of file uploaded so that it does not clas with a pre-existing file. 
+#Use secrets module to randomize image name
+#Use os models to store image type, as image type that was added
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_FN = random_hex + f_ext
+    #store path that image is found in. app.root_path gives full path up to package directory
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_FN)
+    #resize image before saving (from Pillow module)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    #saves image
+    i.save(picture_path)
+    prev_picture = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
+    if os.path.exists(prev_picture) and os.path.basename(prev_picture) != 'default.jpg':
+        os.remove(prev_picture)
+    return picture_FN
+
 @app.route('/account',  methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        #to save profile picture for user. Use a function to set user's profile picture
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
         flash('your account has been updated', 'success')
         #pay attention to post, get redirect to avoid 'are you sure you want to reload?" message
+        #sends another get request to automatically refresh the page
+        #populates form with updated information when you go to the account page
         return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data =  current_user.username
